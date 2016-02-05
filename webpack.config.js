76f2575f213f626d607c640157f76ba115473143
ankit_soni <ankitprahladsoni@gmail.com>
2016-02-06 01:24:22 +0530

    Initial Commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+'use strict';
+var webpack = require('webpack'),
+  path = require('path');
+
+var APP = __dirname + '/app';
+
+module.exports = {
+  context: APP,
+  entry: {
+    app: ['webpack/hot/dev-server', './core/bootstrap.js']
+  },
+  output: {
+    path: APP,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }, {
+      test: /\.js$/,
+      loader: 'ng-annotate!babel?presets[]=es2015!jshint',
+      exclude: /node_modules|bower_components/
+    }, {
+      test: /\.css$/,
+      loader: "style!css"
+    }, {
+      test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
+      loader: 'file-loader?name=res/[name].[ext]?[hash]'
+    }, {
+      test: /\.html/,
+      loader: 'raw'
+    }, {
+      test: /\.json/,
+      loader: 'json'
+    }]
+  },
+  resolve: {
+    root: APP
+  },
+}
